1204da473edcb098f81d3919fb0daf92692a735b
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-15 13:51:59 +0530

    chanage done in Repository file

--- Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourPackageRepository.java
+++ Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourPackageRepository.java
@@ -3,8 +3,10 @@ package com.ganesh.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ganesh.entity.TourPackage;
+
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
   
--- Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourRepository.java
+++ Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourRepository.java
@@ -1,11 +1,25 @@
 package com.ganesh.repository;
 
+import java.util.List;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ganesh.entity.Tour;
 
-@Repository
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+//public interface TourRepository extends CrudRepository<Tour, Integer> {
+//	
+//	List<Tour> findByTourPackageCode(@Param("code") String code);
+//
+//}
+
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+	
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
 }
