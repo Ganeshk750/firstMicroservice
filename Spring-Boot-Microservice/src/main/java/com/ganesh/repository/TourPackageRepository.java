0ae4e2c825810e75c6606e302254210d3951a26c
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-15 14:44:03 +0530

    Adding HAL Browser Depn & Repo changes

--- Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourPackageRepository.java
+++ Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourPackageRepository.java
@@ -3,14 +3,49 @@ package com.ganesh.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.ganesh.entity.TourPackage;
 
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-  
-	Optional<TourPackage> findByName(String name);
+
+    /**
+     * Find Tour Package by name.
+     *
+     * @param name name of the package
+     * @return Optional of TourPackage
+     */
+    Optional<TourPackage> findByName(@Param("name")String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+//    @Override
+//    @RestResource(exported = false)
+//    default void deleteById(String id);
+    
+    
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
 
 
--- Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourRepository.java
+++ Spring-Boot-Microservice/src/main/java/com/ganesh/repository/TourRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.ganesh.entity.Tour;
 
@@ -18,8 +21,37 @@ import com.ganesh.entity.Tour;
 //
 //}
 
-public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
-	
-	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    /**
+     * Find Tours associated with the Tour Package.
+     *
+     * @param code tour package code
+     * @return List of found tours.
+     */
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
