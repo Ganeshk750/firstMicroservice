7244614360b732b297b5aedf1cbe777ccce4e8fa
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-21 11:23:50 +0530

    formating UserService method

--- Bus-Reservation-Rest-Api/src/main/java/com/ganesh/api/service/UserService.java
+++ Bus-Reservation-Rest-Api/src/main/java/com/ganesh/api/service/UserService.java
@@ -8,13 +8,21 @@ import com.ganesh.api.dto.UserDto;
 import com.ganesh.api.entity.User;
 
 public interface UserService extends UserDetailsService {
+	
     User addUser(UserDto userDto);
+    
     User getUserById(Long id);
+    
     User getUserByEmail(String email);
+    
     List<User> getAllUsers();
+    
     void updateUser(User user);
+    
     void deleteUser(Long id);
+    
     void activateEmail(String activationHash);
+    
     void sendConfirmationEmailToUser(User user, String baseUrl);
 
 }
